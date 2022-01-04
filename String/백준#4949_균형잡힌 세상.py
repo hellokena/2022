47f9f5a0ed2d4387706fd38afbdf71bb464a20a8
hellokena <42730820+hellokena@users.noreply.github.com>
2022-01-04 23:17:14 +0900

    2022-01-04

---
+++ String/백준#4949_균형잡힌 세상.py
@@ -0,0 +1,28 @@
+while True:
+    sentences = input()
+    stack = []
+    if sentences == '.': break # 종료 조건
+    for s in sentences:
+        if s == '(' or s == '[': # 열린 괄호의 경우 stack에 추가
+            stack.append(s)
+        elif s == ')': # 작은 닫는 괄호
+            if stack:
+                if stack[-1] == '(': stack.pop()
+                else:
+                    print('no')
+                    break
+            else:
+                print('no')
+                break
+        elif s == ']': # 큰 닫는 괄호
+            if stack:
+                if stack[-1] == '[': stack.pop()
+                else:
+                    print('no')
+                    break
+            else:
+                print('no')
+                break
+    else: # for-else문
+        if not stack: print('yes')
+        else: print('no')
