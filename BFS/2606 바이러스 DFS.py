f01545b05f3851ddae7e5545344f628bd3a9b1ba
hellokena <42730820+hellokena@users.noreply.github.com>
2022-01-10 20:21:33 +0900

    2022-01-10

---
+++ BFS/2606 바이러스 DFS.py
@@ -0,0 +1,24 @@
+#dfs
+
+def dfs(v):
+    global answer
+    visited[v] = 1 # 방문처리
+    for i in graph[v]:
+        if visited[i] == 0: # 아직 방문하지 않았다면
+            answer += 1
+            dfs(i)
+
+def solution(n,m):
+    dfs(1)
+    print(answer)
+
+n = int(input()) # 컴퓨터 수
+m = int(input()) # 네트워크 수
+graph = [[] for _ in range(n+1)] # 연결리스트
+visited = [0]*(n+1)
+for _ in range(m):
+    x,y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+answer = 0
+solution(n,m)
