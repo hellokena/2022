c446e4319397a96801443713ead9c0b9c771f1b1
hellokena <42730820+hellokena@users.noreply.github.com>
2022-01-13 02:24:33 +0900

    2022-01-13

--- DFS & BFS/2644 촌수계산 BFS.py
+++ DFS & BFS/2644 촌수계산 BFS.py
@@ -32,3 +32,38 @@ for _ in range(r):
     graph[c].append(p)
     graph[p].append(c)
 solution(n,n1,n2,r,graph)
+
+# --------------------------------------------------------
+
+# bfs
+from collections import deque
+
+def solution(n,n1,n2,r,graph):
+    visited = [0]*(n+1)
+    # 관계를 나타내는 변수를 선언할 필요 없음
+    result = [0] * (n + 1)  ##
+    queue = deque()
+    queue.append(n1)
+    visited[n1] = 1
+    while queue:
+        pv = queue.popleft()
+        if pv == n2:  ##
+            print(result[pv])
+            break
+        for i in graph[pv]:
+            if visited[i] == 0:
+                queue.append(i)
+                visited[i] = 1
+                result[i] = result[pv] + 1  ##
+    else:
+        print(-1)
+
+n = int(input()) # 사람수
+n1,n2 = map(int, input().split()) # 찾아야 하는 두 사람
+r = int(input()) # 관계수
+graph = [[] for _ in range(n+1)]
+for _ in range(r):
+    p,c = map(int, input().split())
+    graph[c].append(p)
+    graph[p].append(c)
+solution(n,n1,n2,r,graph)
