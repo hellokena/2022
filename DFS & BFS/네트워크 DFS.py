7135adb96d439d0876b4d8220dd57a6c21ea6116
hellokena <42730820+hellokena@users.noreply.github.com>
2022-01-20 04:44:24 +0900

    2022-01-20

--- DFS & BFS/네트워크 DFS.py
+++ DFS & BFS/네트워크 DFS.py
@@ -19,3 +19,22 @@ def solution(n, computers):
             dfs(n, computers, graph, visited, i)
             answer += 1
     return answer
+
+# ---------------------------
+
+# dfs
+def dfs(n, computers, visited, v):
+    visited[v] = 1 # 방문처리
+    for i in range(n):
+        # 같은 컴퓨터 제외, 연결컴퓨터(1), 방문안했을 때
+        if i != v and computers[v][i] == 1 and visited[i] == 0:
+            dfs(n, computers, visited, i)
+                
+def solution(n, computers):
+    answer = 0
+    visited = [0]*n
+    for i in range(n):
+        if visited[i] == 0:
+            dfs(n, computers, visited, i)
+            answer += 1
+    return answer
