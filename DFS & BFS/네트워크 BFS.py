296f6129869741a0fdf2f6c5b8bacd3b07ae9e1d
hellokena <42730820+hellokena@users.noreply.github.com>
2022-01-20 04:48:36 +0900

    2022-01-20

--- DFS & BFS/네트워크 BFS.py
+++ DFS & BFS/네트워크 BFS.py
@@ -24,3 +24,26 @@ def solution(n, computers):
             bfs(n, computers, graph, visited, i)
             answer += 1
     return answer
+
+# -----------------------------------------------
+
+# bfs
+from collections import deque
+def bfs(n, computers, visited, v):
+    queue = deque()
+    queue.append(v)
+    while queue:
+        nv = queue.popleft()
+        for i in range(n):
+            if n != v and computers[nv][i] == 1 and visited[i] == 0:
+                queue.append(i)
+                visited[i] = 1
+                
+def solution(n, computers):
+    answer = 0
+    visited = [0]*n
+    for i in range(n): # 그래프 순회가 아니라 노드 순회
+        if visited[i] == 0:
+            bfs(n, computers, visited, i)
+            answer += 1
+    return answer
