7e8a56dae8321d426dc3eb7ecc12ba78a5674b8a
hellokena <42730820+hellokena@users.noreply.github.com>
2022-02-18 02:51:38 +0900

    Update -- WEATHER_OBSERVATION_STATION_6.sql

--- SQL/HACKER_RANK/BASIC_SELECT/-- WEATHER_OBSERVATION_STATION_6.sql
+++ SQL/HACKER_RANK/BASIC_SELECT/-- WEATHER_OBSERVATION_STATION_6.sql
@@ -7,3 +7,7 @@ WHERE CITY LIKE 'A%' OR
       CITY LIKE 'I%' OR
       CITY LIKE 'O%' OR
       CITY LIKE 'U%';
+      
+SELECT DISTINCT(CITY)
+FROM STATION
+WHERE left(city,1) in ('a','e','i','o','u')
