78fa170665605fc9f9a202c66aa61c6c0fbfa54a
hellokena <42730820+hellokena@users.noreply.github.com>
2022-02-18 02:39:16 +0900

    LIKE IN %

---
+++ SQL/HACKER_RANK/BASIC_SELECT/-- WEATHER_OBSERVATION_STATION_6.sql
@@ -0,0 +1,9 @@
+-- WHERE something LIKE ('bla%', '%foo%', 'batz%') 불가
+
+SELECT CITY
+FROM STATION
+WHERE CITY LIKE 'A%' OR
+      CITY LIKE 'E%' OR
+      CITY LIKE 'I%' OR
+      CITY LIKE 'O%' OR
+      CITY LIKE 'U%';
