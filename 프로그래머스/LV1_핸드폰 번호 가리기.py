5c9228c09229b89bf27dc5428ba869022a526648
hellokena <42730820+hellokena@users.noreply.github.com>
2022-03-09 07:27:47 +0900

    2022-03-09

---
+++ 프로그래머스/LV1_핸드폰 번호 가리기.py
@@ -0,0 +1,8 @@
+# my
+def solution(phone_number):
+    answer = phone_number.replace(phone_number[:-4], '*'*len(phone_number[:-4]))
+    return answer
+  
+# old
+def solution(phone_number):
+    return phone_number.replace(phone_number[:-4], '*'*(len(phone_number)-4))
