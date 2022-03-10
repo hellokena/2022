b1ebf83185a3788d13aa7403f9a42969cb66a372
hellokena <42730820+hellokena@users.noreply.github.com>
2022-03-10 21:52:46 +0900

    2022-03-10

---
+++ 프로그래머스/LV1_정수 내림차순으로 배치하기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    temp = list(map(int,str(n)))
+    temp.sort(reverse=True)
+    return int(''.join(map(str,temp))
+    
+def solution(n):
+  temp = list(str(n))
+  temp.sort(reverse = True)
+  return int("".join(temp))
+               
+# testcase 2,3, 11 오류 
