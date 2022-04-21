f5f32713a5a6c4cd811f7567af6b257ba9ee2dc2
hellokena <42730820+hellokena@users.noreply.github.com>
2022-04-22 02:46:10 +0900

    2022-04-22

---
+++ DFS & BFS/re/22869 징검다리 건너기(small).py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def bfs(nidx):
+    visited = [0]*(n+1)
+    queue = deque()
+    queue.append(nidx)
+    visited[nidx] = 1
+    while queue:
+        nown = queue.popleft()
+        if nown == n-1: return 'YES'
+        for nextn in range(nown+1, n):
+            power = (nextn-nown)*(1+abs(stones[nown]-stones[nextn]))
+            if power<=k and 0<=nextn<n and visited[nextn] == 0:
+                queue.append(nextn)
+                visited[nextn] = 1
+    return 'NO'
+
+def solution(n,k,stones):
+    print(bfs(0))
+
+n,k = map(int, input().split())
+stones = list(map(int, input().split()))
+solution(n,k,stones)
