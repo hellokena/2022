8e14b002855560b47c9c0155e6933495ec67a92f
hellokena <42730820+hellokena@users.noreply.github.com>
2022-05-01 23:19:50 +0900

    2022-05-01

---
+++ DFS & BFS/re/13549 숨바꼭질 3.py
@@ -0,0 +1,21 @@
+from collections import deque
+def solution(n,k):
+    visited = set()
+    queue = deque()
+    queue.append((n,0))
+    visited.add(n)
+    while queue:
+        now, cnt = queue.popleft()
+        if now == k: print(cnt)
+        if 0<=2*now<=100000 and 2*now not in visited:
+            queue.appendleft((now*2,cnt))
+            visited.add(now*2)
+        if 0<=now-1<=100000 and now-1 not in visited:
+            queue.append((now-1,cnt+1))
+            visited.add(now-1)
+        if 0<=now+1<=100000 and now+1 not in visited:
+            queue.append((now+1,cnt+1))
+            visited.add(now+1)
+            
+n,k = map(int, input().split())
+solution(n,k)
