00f8db32552027195c91f5069e5fc765549b226e
hellokena <42730820+hellokena@users.noreply.github.com>
2022-05-02 02:54:15 +0900

    2022-05-02

---
+++ DFS & BFS/hash/11478 서로 다른 부분 문자열의 개수.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = set()
+    for i in range(len(s)):
+        for j in range(i,len(s)):
+            answer.add(s[i:j+1])
+    print(len(answer))
+
+s = input()
+solution(s)
