5105a78ad5d946e89f7278346d1f67f3fdab22fe
hellokena <42730820+hellokena@users.noreply.github.com>
2022-05-03 01:23:47 +0900

    2022-05-03

--- DFS & BFS/hash/@2002 추월.py
+++ DFS & BFS/hash/@2002 추월.py
@@ -5,6 +5,7 @@ def solution(n,in_,out):
         dict[car] = i # 들어온 순서 기록
     for i in range(n):
         for j in range(i,n):
+            # 뒤에꺼부터 확인
             # 현재 차량이 들어간 순서가 뒤에 나오는 차량들의 인덱스보다 큰 경우 있으면 추월
             # 인덱스가 크면 뒤에 들어온 거니까
             if dict[out[i]] > dict[out[j]]: # 나오는 차량 순서대로 확인
