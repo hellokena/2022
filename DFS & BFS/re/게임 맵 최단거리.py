1ad450aabc172483532ce7e5ca6296728a26cddd
hellokena <42730820+hellokena@users.noreply.github.com>
2022-07-14 01:43:58 +0900

    2022-07-14

---
+++ DFS & BFS/re/게임 맵 최단거리.py
@@ -0,0 +1,19 @@
+from collections import deque
+def solution(maps):
+    h = len(maps)
+    w = len(maps[0])
+    queue = deque()
+    queue.append((0,0))
+    while queue:
+        nowh,noww = queue.popleft()
+        if nowh == h-1 and noww == w-1: 
+            return maps[-1][-1]
+        dh = [-1, 1, 0, 0]
+        dw = [0, 0, -1, 1]
+        for d in range(4):
+            nexth = nowh + dh[d]
+            nextw = noww + dw[d]
+            if 0<=nexth<h and 0<=nextw<w and maps[nexth][nextw] == 1:
+                maps[nexth][nextw] = maps[nowh][noww] + 1
+                queue.append((nexth,nextw))  
+    return -1
