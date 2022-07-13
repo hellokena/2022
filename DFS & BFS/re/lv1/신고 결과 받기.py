00082b3308b42122d0177fdb445ddd1103d9851d
hellokena <42730820+hellokena@users.noreply.github.com>
2022-07-14 02:15:58 +0900

    2022-07-14

---
+++ DFS & BFS/re/lv1/신고 결과 받기.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+def solution(id_list, report, k):
+    users = defaultdict(set) # 각 유저가 신고한 유저
+    mail_cnt = defaultdict(int) # 받을 결과 메일 수
+    
+    for rep in report:
+        a,b = rep.split()
+        users[b].add(a)
+    
+    for u in users:
+        if len(users[u]) >= k: # k번이상 신고받은 유저
+            for i in users[u]: # 신고받은 유저를 신고한 유저 대상 메일 송부
+                mail_cnt[i] += 1
+                
+    answer = []
+    for id in id_list:
+        answer.append(mail_cnt[id])
+    return answer
