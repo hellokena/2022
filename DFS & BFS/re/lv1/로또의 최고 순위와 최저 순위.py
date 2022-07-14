602ff9a1404e48aea35a8616e74993c721fd7937
hellokena <42730820+hellokena@users.noreply.github.com>
2022-07-14 17:37:13 +0900

    Update 로또의 최고 순위와 최저 순위.py

--- DFS & BFS/re/lv1/로또의 최고 순위와 최저 순위.py
+++ DFS & BFS/re/lv1/로또의 최고 순위와 최저 순위.py
@@ -7,3 +7,13 @@ def solution(lottos, win_nums):
             max_cnt += 1
             min_cnt += 1
     return [min(6,7-max_cnt), min(6,7-min_cnt)]
+
+def solution(lottos, win_nums):
+    correct = 0
+    rank = [6,6,5,4,3,2,1]
+    zeros = lottos.count(0)
+    for lotto in lottos:
+        for win_num in win_nums:
+            if lotto == win_num:
+                correct += 1 # 맞힌 갯수
+    return [rank[correct+zeros],rank[correct]]
