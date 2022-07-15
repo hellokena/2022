d929394f144a3ebe9645bd03c46e92169dcdee84
hellokena <42730820+hellokena@users.noreply.github.com>
2022-07-16 04:46:04 +0900

    Create 문자열 다루기 기본.py

---
+++ DFS & BFS/re/lv1/문자열 다루기 기본.py
@@ -0,0 +1,8 @@
+def solution(s):
+    if len(s) in [4,6] and s.isdigit(): return True
+    else: return False
+    
+def solution(s):
+    if (len(s)==4 or len(s)==6) and s.isdigit(): return True
+    else: return False
+# 괄호 중요 # 시간차이없음  
