bede5c110ed117dfa11676adbb1ddd796431390a
hellokena <42730820+hellokena@users.noreply.github.com>
2022-07-19 03:51:42 +0900

    Update [1차] 뉴스 클러스터링.py

--- DFS & BFS/re/lv1/[1차] 뉴스 클러스터링.py
+++ DFS & BFS/re/lv1/[1차] 뉴스 클러스터링.py
@@ -19,3 +19,21 @@ def solution(str1, str2):
             union.append(num)
     if not union: return 65536 # intersect는 없을 수도 있음
     return int(len(intersect)/len(union)*65536)
+    
+
+# 자카드 유사도 : 교집합 / 합집합
+def solution(str1, str2):
+    array1, array2 = [], []
+    for i in range(len(str1)-1):
+        if str1[i:i+2].isalpha(): array1.append(str1[i:i+2].lower())
+    for i in range(len(str2)-1):
+        if str2[i:i+2].isalpha(): array2.append(str2[i:i+2].lower())
+    #if len(array1) == 0 and len(array2) == 0: return 65536
+    all_num = set(array1+array2)
+    intersect = []
+    union = []
+    for num in all_num:
+        intersect.extend([num]*min(array1.count(num),array2.count(num)))
+        union.extend([num]*max(array1.count(num), array2.count(num)))
+    if not union: return 65536 # intersect는 없을 수도 있음
+    return int(len(intersect)/len(union)*65536)
